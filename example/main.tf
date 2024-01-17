a2560a4f7029feee9640d82dc3569066d3aa66bf
Jason Paige <jasonpaige@users.noreply.github.com>
2024-01-17 11:48:55 +0000

    Fix example

--- example/main.tf
+++ example/main.tf
@@ -1,9 +1,10 @@
 module "apim-subscription" {
-  api_management_name = "cft-api-mgmt-sbox"
-  resource_group_name = "cft-sbox-network-rg"
-  display_name        = "Test Subscription"
-  product_id          = module.apim_user.id
-  user_id             = module.apim_product.id
-  state               = "active"
-  allowing_tracing    = var.env == "stg" || var.env == "demo" ? true : false
+  source           = "git@github.com:hmcts/cnp-module-api-mgmt-subscription?ref=master"
+  api_mgmt_name    = "cft-api-mgmt-sbox"
+  api_mgmt_rg      = "cft-sbox-network-rg"
+  sub_display_name = "Test Subscription"
+  product_id       = module.apim_user.id
+  user_id          = module.apim_product.id
+  state            = "active"
+  allow_tracing    = var.env == "stg" || var.env == "demo" ? true : false
 }
